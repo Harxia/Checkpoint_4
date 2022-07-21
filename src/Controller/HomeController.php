f9fd8c45acebce9000e8cc9570341f8de76488fb
Harxia <jonathan.matulla@gmail.com>
2022-07-21 12:45:40 +0200

    Affichage dynamique à la BDD / Création des Entités / BDD

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,28 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\ResumeRepository;
+use App\Repository\ExperienceRepository;
+use App\Repository\EducationRepository;
+use App\Repository\SchoolRepository;
+use App\Repository\ContactRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
-    {
+    public function index(
+        ResumeRepository $resumeRepository,
+        ExperienceRepository $experienceRepository,
+        EducationRepository $educationRepository,
+        SchoolRepository $schoolRepository,
+        ContactRepository $contactRepository
+    ): Response {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'resume' => $resumeRepository->findOneBy(['id' => 1]),
+            'experience' => $experienceRepository->findOneBy(['id' => 1]),
+            'education' => $educationRepository->findOneBy(['id' => 1]),
+            'school' => $schoolRepository->findOneBy(['id' => 1]),
+            'contact' => $contactRepository->findOneBy(['id' => 1]),
         ]);
     }
 }
---
+++ src/Entity/School.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SchoolRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SchoolRepository::class)]
+class School
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column()]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $category = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $debut = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $fin = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getCategory(): ?string
+    {
+        return $this->category;
+    }
+
+    public function setCategory(string $category): self
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    public function getDebut(): ?string
+    {
+        return $this->debut;
+    }
+
+    public function setDebut(string $debut): self
+    {
+        $this->debut = $debut;
+
+        return $this;
+    }
+
+    public function getFin(): ?string
+    {
+        return $this->fin;
+    }
+
+    public function setFin(string $fin): self
+    {
+        $this->fin = $fin;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
