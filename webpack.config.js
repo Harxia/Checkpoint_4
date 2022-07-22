f78cd0b095177981bb48f434ea09a7bd2425fa8a
Harxia <jonathan.matulla@gmail.com>
2022-07-22 09:37:07 +0200

    Crud Style SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ Encore
     .addEntry('app', './assets/app.js')
     .addEntry('home', './assets/home.js')
     .addEntry('login', './assets/login.js')
+    .addEntry('crud', './assets/crud.js')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
